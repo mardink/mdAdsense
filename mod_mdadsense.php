c4c90690573a64866ff72928f38e9098da3be4a9
mardink <support@mardinkwebdesign.com>
2012-12-11 19:00:19 +0100

    Update mod_mdadsense.php
    changes

--- mod_mdadsense.php
+++ mod_mdadsense.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Joomla
- * @subpackage mddonate
- * @copyright (C) 2011 Martijn Hiddink
+ * @subpackage mdadsense
+ * @copyright (C) 2012 Martijn Hiddink
  * @license GNU/GPL, 
- * mddonate is free software; you can redistribute it and/or
+ * mdadsense is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License 3
  * as published by the Free Software Foundation.
 
- * mddonate is distributed in the hope that it will be useful,
+ * mdadsense is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -16,6 +16,6 @@
 
 	// no direct access
 	defined( '_JEXEC' ) or die( 'Restricted access' );
-	require( JModuleHelper::getLayoutPath( 'mod_mddonate' ) );
+	require( JModuleHelper::getLayoutPath( 'mod_mdadsense' ) );
 
 ?>
