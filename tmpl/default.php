27c8214840aff687b5255dee5a433bafd5c63e51
Mardink Webdesign <support@mardinkwebdesign.com>
2012-12-13 21:58:01 +0100

    added options in backend

--- tmpl/default.php
+++ tmpl/default.php
@@ -6,4 +6,27 @@
 	if ($langSite != '') {
 		$langSite .= '/';
 	}
+// get google adsense conde
+	$adsense = $params->get('adsense');
 	
+// Define alignment
+	$align = $params->get('align');
+		if ($align == "1") { $alignpos = 'pull-left';} else {
+		if ($align == "2") { $alignpos = 'center';} else { $alignpos = 'pull-right';}
+	}
+// define devices to show	
+	$show = $params->get('show');
+	if ($show == "1") { $showstate = 'visible';} else {
+		 $showstate = 'hidden';	}
+	$device = $params->get('device');
+		 if ($device == "1") { $device1 = '-phone';} else {
+		 	if ($device == "2") { $device1 = '-tablet';} else { $device1 = '-desktop';}
+		 }
+$showdevice = "$showstate" . "$device1";
+?>
+<div class="<?php echo $showdevice;?>">
+	<div class="<?php echo $alignpos;?>">
+		<?php echo $adsense;?>
+<p></p>
+	</div>
+</div>
